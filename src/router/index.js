7809d0387250fc1591ed49c1e1685daf34d9e5d6
Neiland85 <bookingnadarecords@gmail.com>
2024-08-13 01:29:24 +0200

    Feature: Configured Vue Router with routes for ProductList, ProductDetail, and Cart

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,33 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import VueRouter from 'vue-router';
 import ProductList from '../components/ProductList.vue';
 import ProductDetail from '../components/ProductDetail.vue';
 import Cart from '../components/Cart.vue';
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
+const routes = [
+  {
+    path: '/',
+    name: 'ProductList',
+    component: ProductList,
+  },
+  {
+    path: '/products/:id',
+    name: 'ProductDetail',
+    component: ProductDetail,
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart,
+  },
+];
+
+const router = new VueRouter({
   mode: 'history',
-  routes: [
-    { path: '/', component: ProductList },
-    { path: '/products/:id', component: ProductDetail },
-    { path: '/cart', component: Cart },
-  ],
+  routes,
 });
+
+export default router;
 
